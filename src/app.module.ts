7e569817a1b69f353a30232be82bdfed4c97937f
jookwonyoung <jooky0327@naver.com>
2022-11-10 23:33:29 +0900

    2-4까지 re

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,17 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV ==='dev' ? '.dev.env' : '.test.env',
     }),
     TypeOrmModule.forRoot({
       type: "postgres",
